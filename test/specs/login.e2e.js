e326e7bfaa1aa3a41601b50a1117e97ee416d780
notsonoobie <swastikmedical74@gmail.com>
2021-08-02 18:12:22 +0530

    Url Changes.

--- test/specs/login.e2e.js
+++ test/specs/login.e2e.js
@@ -2,7 +2,7 @@ const { assert } = require("chai");
 
 describe('ATV Mweb Login Test', () => {
     it('should login with valid Airtel MSISDN and enter into Featured Tab', async () => {
-        await browser.url(`http://localhost:3000/login`);
+        await browser.url(`/login`);
 
         await $('input').setValue('7304105228');
         await $('button[type="button"]').click();
